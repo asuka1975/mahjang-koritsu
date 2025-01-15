19414f834a5ffa7bb777e4230335a12348bb2bdc
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2025-01-16 07:57:40 +0900

    feature: dist

--- src/utils/answerUtils.ts
+++
@@ -1,5 +0,0 @@
-import { Question } from "../types/types";
-
-export function judgeAnswer(choiceIndex: number, question: Question): boolean {
-    return [question.katachi1, question.katachi2][choiceIndex].order > [question.katachi1, question.katachi2][1 - choiceIndex].order;
-}
