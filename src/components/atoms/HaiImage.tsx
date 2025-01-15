55a451d307363e943ab92f33869dc91d07f60832
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2025-01-16 08:23:33 +0900

    feature: change base url

--- src/components/atoms/HaiImage.tsx
+++ src/components/atoms/HaiImage.tsx
@@ -1,20 +1,31 @@
 import { Hai } from "../../types/types"
 
+import m1 from "../../assets/m1.png"
+import m2 from "../../assets/m2.png"
+import m3 from "../../assets/m3.png"
+import m4 from "../../assets/m4.png"
+import m5 from "../../assets/m5.png"
+import m6 from "../../assets/m6.png"
+import m7 from "../../assets/m7.png"
+import m8 from "../../assets/m8.png"
+import m9 from "../../assets/m9.png"
+import j3 from "../../assets/j3.png"
+
 type Props = {
     hai: Hai
 }
 
 const images = {
-    1: "m1.png",
-    2: "m2.png",
-    3: "m3.png",
-    4: "m4.png",
-    5: "m5.png",
-    6: "m6.png",
-    7: "m7.png",
-    8: "m8.png",
-    9: "m9.png",
-    "西": "j3.png"
+    1: m1,
+    2: m2,
+    3: m3,
+    4: m4,
+    5: m5,
+    6: m6,
+    7: m7,
+    8: m8,
+    9: m9,
+    "西": j3
 }
 
 export function HaiImage({ hai }: Props) {
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  let base = "/"
+  if (mode === "production") {
+    base = "/mahjang-koritsu/"
+  }
+
+  return {
+    plugins: [react()],
+    base: base
+  }
 })
